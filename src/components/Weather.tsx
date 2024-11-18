81ef0f25343f17214942ec4885d51bce5f6a50da
Sagar <desiferchef@gmail.com>
2024-11-18 11:54:50 +0545

    Create object literal for weather code icons

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -10,3 +10,26 @@ import rain_icon from "../assets/images/rain.png";
 import snow_icon from "../assets/images/snow.png";
 import wind_icon from "../assets/images/wind.png";
 import humidity_icon from "../assets/images/humidity.png";
+
+function Weather() {
+    const inputRef = useRef();
+    const [weatherData, setWeatherData] = useState(false);
+
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n": snow_icon,
+    };
+
+    
+}
