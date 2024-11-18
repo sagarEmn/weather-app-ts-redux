e213419bb2b5cd1d15f933b2a8402cfd4269ac5c
Sagar <desiferchef@gmail.com>
2024-11-18 11:53:37 +0545

    Import images

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,3 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Weather.css";
 
+// import images
+import search_icon from "../assets/images/search.png";
+import clear_icon from "../assets/images/clear.png";
+import cloud_icon from "../assets/images/cloud.png";
+import drizzle_icon from "../assets/images/drizzle.png";
+import rain_icon from "../assets/images/rain.png";
+import snow_icon from "../assets/images/snow.png";
+import wind_icon from "../assets/images/wind.png";
+import humidity_icon from "../assets/images/humidity.png";
